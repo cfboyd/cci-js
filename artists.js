d579f4f16e5620ed70d89907846fac1d3647e8cf
cfboyd <cfboyd@gmail.com>
2017-01-24 19:48:53 -0800

    artists.js first commit

---
+++ artists.js
@@ -0,0 +1,14 @@
+switch (prompt("Give us a name and we'll give you an artist they like")) {
+  case "colin":
+    console.log("Japanese pop artists Maywa Denki");
+    break;
+  case "alden":
+    console.log("@BadlandsNPS on Twitter spitting science facts");
+    break;
+  case "sea":
+    console.log("Home Land Security, the new exhibit by the For-Site Foundation");
+    break;
+  case "christian":
+    console.log("NASA video of solar flares in slow motion");
+    break;
+}
